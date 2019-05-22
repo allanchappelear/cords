8149b033b269231cf9c0e9047b5e421d4379cd87
Allan Chappelear <allanc@newtarget.com>
2019-05-22 02:40:17 -0400

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,5 +63,74 @@ $( document ).ready(function() {
 	$('#header-intro').trigger('play');
     // Second Slide Animations
 
+    var ctx = document.getElementById('myChart').getContext('2d');
+	var myChart = new Chart(ctx, {
+	    type: 'line',
+	    data: {
+	        labels: ['1960', '1970', '1980', '1990', '2000', '2010'],
+	        datasets: [{
+	            label: '# Space Objects',
+	            data: [0, 100, 250, 500, 1200, 2000],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255, 99, 132, 1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }]
+	        }
+	    }
+	});
+
+
+	var byCountryx = document.getElementById('byCountry').getContext('2d');
+	var byCountry = new Chart(byCountryx, {
+	    type: 'doughnut',
+	    data: {
+	        labels: ['US', 'China', 'Russia', 'Multinational', 'Japan', 'UK'],
+	        datasets: [{
+	            label: 'Space Objects By Country',
+	            data: [830, 280, 140, 145, 75, 54],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255, 99, 132, 1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+
+	});
+
 
 });
