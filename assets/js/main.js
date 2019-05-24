f12dd25c27107db78b63a4c0b1e74c11d06b70f6
Allan Chappelear <allanc@newtarget.com>
2019-05-24 13:35:12 -0400

    updated scrolling stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,150 +1,188 @@
 // variables
+$('body').css('overflow', 'hidden');
+
+
+//Check Viewport
+$.fn.visible = function(partial) {
+
+	var $t            = $(this),
+	$w            = $(window),
+	viewTop       = $w.scrollTop(),
+	viewBottom    = viewTop + $w.height()-200,
+	_top          = $t.offset().top,
+	_bottom       = _top + $t.height(),
+	compareTop    = partial === true ? _bottom : _top,
+	compareBottom = partial === true ? _top : _bottom;
+
+	return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+};
+
+
+$(window).on('load', function() {
+	setTimeout(function(){
+		$('#loader').fadeOut('slow');
+	}, 1000);
+	$('#section0 #header-intro').addClass('in');
+	$('#section0 #header-intro')[0].play();
+
+	
+});
+
+$(window).on('scroll', function(e) {
+	 var scroll = $(this).scrollTop();
+            var opacity = 1 - (scroll / 1000);
+            var headingParallax = -scroll / 6;
+            $('#heading').css({transform: 'translateY(' + headingParallax +'px)'});
+            $('p.lead').css({transform: 'translateY(' + headingParallax +'px)'});
+
+            if (opacity > 0) {
+                $('.header').css('opacity', opacity);
+                
+            }
+	$(".is-animated").each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+			el.addClass("animated fadeInUp"); 
+		}
+	});
+	var inView = false;
+	 if($('#chart').visible(true)) {
+        if (inView) { return; }
+        inView = true;
+
+        	var spaceObj = document.getElementById('myChart').getContext('2d');
+		    var myChart = new Chart(spaceObj, {
+		    	type: 'line',
+		    	data: {
+		    		labels: ['1960', '1970', '1980', '1990', '2000', '2010'],
+		    		datasets: [{
+		    			label: '# Space Objects',
+		    			data: [0, 100, 250, 500, 1200, 2000],
+		    			backgroundColor: [
+		    			'rgba(255, 99, 132, 0.2)',
+		    			'rgba(54, 162, 235, 0.2)',
+		    			'rgba(255, 206, 86, 0.2)',
+		    			'rgba(75, 192, 192, 0.2)',
+		    			'rgba(153, 102, 255, 0.2)',
+		    			'rgba(255, 159, 64, 0.2)'
+		    			],
+		    			borderColor: [
+		    			'rgba(255, 99, 132, 1)',
+		    			'rgba(54, 162, 235, 1)',
+		    			'rgba(255, 206, 86, 1)',
+		    			'rgba(75, 192, 192, 1)',
+		    			'rgba(153, 102, 255, 1)',
+		    			'rgba(255, 159, 64, 1)'
+		    			],
+		    			borderWidth: 1
+		    		}]
+		    	},
+		    	options: {
+		    		scales: {
+		    			yAxes: [{
+		    				ticks: {
+		    					beginAtZero: true
+		    				}
+		    			}]
+		    		}
+		    	}
+		    });
+    } else {
+        inView = false;  
+    }
+    console.log(inView);
+	
+});
+
+
+
 
 $( document ).ready(function() {
-  	var $isAnimatedSecond = $('#section1 .is-animated');
-  	var $isAnimatedThird = $('#section2 .is-animated');
-  	var $isAnimated4 = $('#section3 .is-animated');
-
-  	var $debris = $('#section2 .debris');
 
 
-    
+
 
     // FULL PAGE
-	var myFullpage = new fullpage('#fullpage', {
-      licenseKey: '55EF54C5-79804EFB-8D4587F8-748A27E9',
-      anchors: ['Intro','History','Problem','Solution', 'Rentry'],
+    var myFullpage = new fullpage('#fullpage', {
+    	licenseKey: '55EF54C5-79804EFB-8D4587F8-748A27E9',
+    	anchors: ['Intro','History','Problem','Solution', 'Rentry'],
         //sectionsColor: ['#333', '#1BBC9B', '#7E8F7C'],
         navigation: true,
         navigationPosition: 'left',
         navigationTooltips: ['Intro', 'History', 'The Problem', 'The Solution', 'Rentry'],
-        scrollOverflow: true,
+        //scrollOverflow: true,
         css3: true,
-      	showActiveTooltip: true,
-  		scrollBar: false,
+        showActiveTooltip: true,
+  		//scrollBar: true,
+  		autoScrolling:false,
   		//autoScrolling: true,
 		//normalScrollElements: '#section0, #section1, #section2, #section3, #section4',
 		//scrollOverflow: false,
 		fadingEffect: true,
+		fitToSection:false,
 		//normalScrollElements: '#section0, #section1',
 
-        onLeave: function(index, nextIndex, direction) {
-        	console.log(index);
-        	if(index.anchor == "History") {
-  				$('#section0 #header-intro')[0].play();
-        	} else {
-        		$('#section0 #header-intro')[0].pause();
-        	}
-        	if(index.index === 0 ) {
-        		$isAnimatedSecond.addClass('animated fadeInUp'); 
-		        $isAnimatedSecond.eq(0).css('animation-delay', '.3s');
-		        $isAnimatedSecond.eq(1).css('animation-delay', '.6s');
-		        $isAnimatedSecond.eq(3).css('animation-delay', '.9s');
-   		        $isAnimatedSecond.eq(4).css('animation-delay', '1.2s');	
-
-        	} else if(index.index == 1) {
-		        $isAnimatedThird.addClass('animated fadeInUp').css('animation-delay', '.3s');
-		        $isAnimatedThird.eq(1).css('animation-delay', '1s');
-		        $isAnimatedThird.eq(2).css('animation-delay', '2s');
-		       	$debris.addClass('show').css('animation-delay', '.5s');
-		       	$debris.eq(1).css('animation-delay', '1s');
-		       	$debris.eq(2).css('animation-delay', '2s');
-		       	$debris.eq(3).css('animation-delay', '3s');
-		       	$debris.eq(4).css('animation-delay', '4s');
-		       	$debris.eq(5).css('animation-delay', '5s');
-
-		      } else if(index.index == 2) {
-		        $isAnimated4.addClass('animated fadeInUp').css('animation-delay', '.3s');
-		        $isAnimated4.eq(0).css('animation-delay', '.6s');
-
-		      }
-        }
-     });
-	
-		//First Slide Animations
-	setTimeout(function(){
-		$('#section0 #header-intro').addClass('in');
-		$('#section0 #header-intro')[0].play();
-	}, 1000);
-	setTimeout(function(){
-		$('#section0 p').addClass('in');
-	}, 10000);
-
-	setTimeout(function(){
-		$('#section0 h1').addClass('in');
-	}, 7000);
-	
-	$('#header-intro').trigger('play');
-    // Second Slide Animations
-
-    
-var ctx = document.getElementById('myChart').getContext('2d');
-					var myChart = new Chart(ctx, {
-				    type: 'line',
-				    data: {
-				        labels: ['1960', '1970', '1980', '1990', '2000', '2010'],
-				        datasets: [{
-				            label: '# Space Objects',
-				            data: [0, 100, 250, 500, 1200, 2000],
-				            backgroundColor: [
-				                'rgba(255, 99, 132, 0.2)',
-				                'rgba(54, 162, 235, 0.2)',
-				                'rgba(255, 206, 86, 0.2)',
-				                'rgba(75, 192, 192, 0.2)',
-				                'rgba(153, 102, 255, 0.2)',
-				                'rgba(255, 159, 64, 0.2)'
-				            ],
-				            borderColor: [
-				                'rgba(255, 99, 132, 1)',
-				                'rgba(54, 162, 235, 1)',
-				                'rgba(255, 206, 86, 1)',
-				                'rgba(75, 192, 192, 1)',
-				                'rgba(153, 102, 255, 1)',
-				                'rgba(255, 159, 64, 1)'
-				            ],
-				            borderWidth: 1
-				        }]
-				    },
-				    options: {
-				        scales: {
-				            yAxes: [{
-				                ticks: {
-				                    beginAtZero: true
-				                }
-				            }]
-				        }
-				    }
-				});
-
-	var byCountryx = document.getElementById('byCountry').getContext('2d');
-	var byCountry = new Chart(byCountryx, {
-	    type: 'doughnut',
-	    data: {
-	        labels: ['US', 'China', 'Russia', 'Multinational', 'Japan', 'UK'],
-	        datasets: [{
-	            label: 'Space Objects By Country',
-	            data: [830, 280, 140, 145, 75, 54],
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
-	            ],
-	            borderColor: [
-	                'rgba(255, 99, 132, 1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	                'rgba(255, 159, 64, 1)'
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-
+		
 	});
 
+		//First Slide Animations
+		setTimeout(function(){
+			$('#section0 #header-intro').addClass('in');
+			$('#section0 #header-intro')[0].play();
+		}, 1000);
+		
+		setTimeout(function(){
+			$('#section0 #debris').addClass('in');
+		}, 1000);
+		setTimeout(function(){
+			$('#section0 #debris').fadeOut(1000);
+		}, 6000);
 
-});
+		setTimeout(function(){
+			$('#section0 p').addClass('in');
+
+		}, 10000);
+
+		setTimeout(function(){
+			$('#section0 h1').addClass('in');
+		}, 7000);
+		
+		$('#header-intro').trigger('play');
+	    // Second Slide Animations
+
+
+	    
+
+	    var byCountryx = document.getElementById('byCountry').getContext('2d');
+	    var byCountry = new Chart(byCountryx, {
+	    	type: 'doughnut',
+	    	data: {
+	    		labels: ['US', 'China', 'Russia', 'Multinational', 'Japan', 'UK'],
+	    		datasets: [{
+	    			label: 'Space Objects By Country',
+	    			data: [830, 280, 140, 145, 75, 54],
+	    			backgroundColor: [
+	    			'rgba(255, 99, 132, 0.2)',
+	    			'rgba(54, 162, 235, 0.2)',
+	    			'rgba(255, 206, 86, 0.2)',
+	    			'rgba(75, 192, 192, 0.2)',
+	    			'rgba(153, 102, 255, 0.2)',
+	    			'rgba(255, 159, 64, 0.2)'
+	    			],
+	    			borderColor: [
+	    			'rgba(255, 99, 132, 1)',
+	    			'rgba(54, 162, 235, 1)',
+	    			'rgba(255, 206, 86, 1)',
+	    			'rgba(75, 192, 192, 1)',
+	    			'rgba(153, 102, 255, 1)',
+	    			'rgba(255, 159, 64, 1)'
+	    			],
+	    			borderWidth: 1
+	    		}]
+	    	},
+
+	    });
+
+
+	});
